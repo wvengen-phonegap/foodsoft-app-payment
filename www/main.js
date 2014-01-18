815928eb376d7decc6b322ffc3f07622b5b48a42
wvengen <dev-voko@willem.engen.nl>
2014-01-18 21:31:55 +0100

    cleanup, better integration with foodsoft_adyen

--- www/main.js
+++ www/main.js
@@ -12,8 +12,12 @@ function handleOpenURL(url) {
   setTimeout(function() {
     switch(url.attr('host')) {
     case 'set-default': 
-      if (setDefault(url.param('base'), url.param('path'), url.param('key')))
+      if (setDefault(url.param('base'), url.param('path'), url.param('token')))
         visitSite();
+      break;
+    case 'payment-return':
+      // on returning from a payment, redirect to that page
+      visitUrl(decodeURI(url.attr('path')) + '?' + url.attr('query'));
       break;
     default:
       // TODO leave app again
@@ -32,32 +36,42 @@ function visitSite(path) {
     var path = pref.get('foodsoft_path');
     if (!path) path = 'payments/adyen/pin';
   }
-  if (window.localStorage.getItem('foodsoft_key')) {
-    path += '?key=' + pref.get('foodsoft_key');
-  }
+  return visitUrl(url+'/'+path);
+}
 
+// open url in iframe (with hooks)
+function visitUrl(url) {
+  // always set mobile_app parameter to indicate we're the app
+  if (url.indexOf('?')>0)
+    var url = url + '&amp;mobile_app=1';
+  else
+    var url = url + '?mobile_app=1';
+  // and include token (now we have a query string already)
+  if (pref.get('foodsoft_token'))
+    url += '&amp;token='+encodeURI(pref.get('foodsoft_token'));
+  // open in iframe
   $.mobile.changePage('#page-site', {changeHash: false});
   $.mobile.loading('show');
   $('#site-frame').load(function() {
     $.mobile.loading('hide');
   });
-  $('#site-frame').attr('src', url+'/'+path);
+  $('#site-frame').attr('src', url);
   return true;
 }
 
 // set site location
-function setDefault(base, path, key) {
+function setDefault(base, path, token) {
   if (!base) {
     alert('No URL given');
     return false;
   }
-  if (!key) {
-    alert('No key given');
+  if (!token) {
+    alert('No token given');
     return false;
   }
   pref.set('foodsoft_base', base);
   pref.set('foodsoft_path', path);
-  pref.set('foodsoft_key', key);
+  pref.set('foodsoft_token', token);
   return true;
 }
 
